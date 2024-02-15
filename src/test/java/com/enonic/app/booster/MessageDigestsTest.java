2011315ea812809dba9396f4d67b702541bd7f36
rymsha <rymsha@yandex.ru>
2024-02-15 15:22:17 +0100

    more tests

---
+++ src/test/java/com/enonic/app/booster/MessageDigestsTest.java
@@ -0,0 +1,18 @@
+package com.enonic.app.booster;
+
+import java.util.HexFormat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MessageDigestsTest
+{
+
+    @Test
+    void sha256()
+    {
+        assertEquals( "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                      HexFormat.of().formatHex( MessageDigests.sha256().digest() ) );
+    }
+}
