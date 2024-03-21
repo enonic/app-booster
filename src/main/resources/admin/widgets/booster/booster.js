fbdab672388a99e9653ebd6e0507b3a580fd6a0b
alansemenov <ase@enonic.com>
2024-03-21 13:13:31 +0100

    Check for allowed roles (#25)

--- src/main/resources/admin/widgets/booster/booster.js
+++ src/main/resources/admin/widgets/booster/booster.js
@@ -1,16 +1,47 @@
 const portal = require('/lib/xp/portal');
 const contentLib = require('/lib/xp/content');
+const contextLib = require('/lib/xp/context');
+const authLib = require('/lib/xp/auth');
 const mustache = require('/lib/mustache');
 const licenseManager = require("/lib/license-manager");
 
 const forceArray = (data) => (Array.isArray(data) ? data : new Array(data));
+
+const allowedRoles = ['role:system.admin', 'role:cms.admin'];
+
+const hasAllowedRole = () => {
+    let hasAllowedRole = false;
+    allowedRoles.concat(getProjectOwnerRole()).forEach(role => {
+        if (authLib.hasRole(role)) {
+            log.info('Current user has role: ' + role);
+            hasAllowedRole = true;
+        }
+    });
+    return hasAllowedRole;
+}
+
+const getProjectOwnerRole = () => {
+    const context = contextLib.get();
+    if (!context || !context.repository) {
+        return '';
+    }
+    const project = context.repository.replace('com.enonic.cms.', '');
+    if (!project) {
+        return null;
+    }
+    return `role:cms.project.${project}.owner`;
+}
 
 const isAppEnabledOnSite = (contentId) => {
     if (!contentId) {
         return true;
     }
     const site = contentLib.getSite({ key: contentId });
-    if (!site || !site.data || !site.data.siteConfig) {
+    if (!site) {
+        return true;
+    }
+
+    if (!site.data || !site.data.siteConfig) {
         return false;
     }
 
@@ -25,32 +56,43 @@ const isAppEnabledOnSite = (contentId) => {
 }
 
 const renderWidgetView = (req) => {
-    const contentId = req.params.contentId || '';
-    const project = req.params.repository.replace('com.enonic.cms.', '') || '';
     let contentPath = '';
     let isSiteSelected = false;
     let isContentSelected = false;
-
-    if (contentId) {
-        const content = contentLib.get({
-            key: contentId
-        });
-        contentPath = content._name;
-        isSiteSelected = content.type === 'portal:site';
-        isContentSelected = !isSiteSelected;
-    }
-
-    let nodeCleanerBean = __.newBean('com.enonic.app.booster.storage.NodeCleanerBean');
+    let errorMessage;
     let size;
-    if (contentId) {
-        if (isSiteSelected) {
-            size = nodeCleanerBean.getSiteCacheSize(project, contentId);
+
+    const contentId = req.params.contentId || '';
+    const project = req.params.repository.replace('com.enonic.cms.', '') || '';
+
+    if (hasAllowedRole()) {
+        if (contentId) {
+            const content = contentLib.get({
+                key: contentId
+            });
+            contentPath = content._name;
+            isSiteSelected = content.type === 'portal:site';
+            isContentSelected = !isSiteSelected;
+        }
+
+        let nodeCleanerBean = __.newBean('com.enonic.app.booster.storage.NodeCleanerBean');
+        if (contentId) {
+            if (isSiteSelected) {
+                size = nodeCleanerBean.getSiteCacheSize(project, contentId);
+            } else {
+                size = nodeCleanerBean.getContentCacheSize(project, contentId);
+            }
         } else {
-            size = nodeCleanerBean.getContentCacheSize(project, contentId);
+            size = nodeCleanerBean.getProjectCacheSize(project);
+        }
+
+        if (!isAppEnabledOnSite(contentId)) {
+            errorMessage = 'Booster app is not added to the site';
         }
     } else {
-        size = nodeCleanerBean.getProjectCacheSize(project);
+        errorMessage = 'You do not have permission to access this application';
     }
+
 
     const view = resolve('booster.html');
     const params = {
@@ -62,11 +104,12 @@ const renderWidgetView = (req) => {
         size,
         isButtonDisabled: size === 0,
         isProjectSelected: !isContentSelected && !isSiteSelected,
-        isEnabled: isAppEnabledOnSite(contentId),
+        isEnabled: !errorMessage,
         assetsUri: portal.assetUrl({ path: ''}),
         serviceUrl: portal.serviceUrl({ service: 'booster' }),
         isLicenseValid: licenseManager.isLicenseValid(),
-        licenseUploadUrl: portal.serviceUrl({ service: 'license-upload' })
+        licenseUploadUrl: portal.serviceUrl({ service: 'license-upload' }),
+        errorMessage
     };
 
     return {
