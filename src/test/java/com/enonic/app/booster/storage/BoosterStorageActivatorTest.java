1d1a7542c07b66935faebc2ed2cabd2eaf14d7cc
rymsha <sry@enonic.com>
2025-03-05 08:29:06 +0100

    Transient repository #99 fix test

--- src/test/java/com/enonic/app/booster/storage/BoosterStorageActivatorTest.java
+++ src/test/java/com/enonic/app/booster/storage/BoosterStorageActivatorTest.java
@@ -2,6 +2,7 @@ package com.enonic.app.booster.storage;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 import com.enonic.xp.index.IndexService;
@@ -9,6 +10,7 @@ import com.enonic.xp.node.NodeService;
 import com.enonic.xp.repository.CreateRepositoryParams;
 import com.enonic.xp.repository.RepositoryService;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -30,7 +32,10 @@ class BoosterStorageActivatorTest
         when( indexService.isMaster() ).thenReturn( true );
         when( indexService.waitForYellowStatus() ).thenReturn( true );
         new BoosterStorageActivator( indexService, repositoryService, nodeService );
-        verify( repositoryService ).createRepository(
-            CreateRepositoryParams.create().repositoryId( BoosterContext.REPOSITORY_ID ).build() );
+        ArgumentCaptor<CreateRepositoryParams> paramsCaptor = ArgumentCaptor.forClass( CreateRepositoryParams.class );
+        verify( repositoryService ).createRepository( paramsCaptor.capture() );
+
+        assertThat( paramsCaptor.getValue() ).extracting( CreateRepositoryParams::getRepositoryId, CreateRepositoryParams::isTransient )
+            .containsExactly( BoosterContext.REPOSITORY_ID, true );
     }
 }
